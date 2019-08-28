8ea32e0cf562e3d7f45a940362c6dd4793e4dab0
Morgan Nicol <morganichoui@gmail.com>
2019-08-28 17:10:48 +0200

    powered by quichaud modif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatButtonModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatTabsModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ConvertComponent } from './convert/convert.component';
 import { BourseComponent } from './convert/bourse/bourse.component';
 import { NavbarComponent } from './convert/navbar/navbar.component';
 import { MatIconModule } from '@angular/material/icon';
+import { QuichaudComponent } from './convert/quichaud/quichaud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConvertComponent,
     BourseComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuichaudComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     FormsModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/convert/quichaud/quichaud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'uc-quichaud',
+  templateUrl: './quichaud.component.html',
+  styleUrls: ['./quichaud.component.scss']
+})
+export class QuichaudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
